76b9996c30f2bbb775c2c084f673dba1d17380ce
Guilherme Cabrita <guilherme.cabrita.t0129862@edu.atec.pt>
2025-10-10 08:00:09 +0100

    ex1 finalizado

--- Exercicios/ex1.py
+++ Exercicios/ex1.py
@@ -2,8 +2,10 @@
 
 segs = int(input("Insira os segundos para a conversão: "))
 
-hrs = segs / 3600
-min = segs / 60
+hrs = segs // 3600
+segs_restantes = segs % 3600
+min = segs_restantes // 60
+segs2 = segs_restantes % 60
 
-print("Horas:", hrs, "")
+print("Horas:", hrs, "  Min:", min, "  Seg:", segs2)
 
